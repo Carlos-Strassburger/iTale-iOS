a5f2f986063aa873d111d0047cf57215b9de72ab
Carlos Filho <strassburger.filho@gmail.com>
2023-04-04 18:04:27 -0300

    adding the base project

---
+++ iTale/iTale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTale
+//
+//  Created by Carlos Strassburger Filho on 04/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
