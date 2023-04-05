8bdd98d56a31ed816e609c3ba1667ea33a230df3
Carlos Filho <strassburger.filho@gmail.com>
2023-04-05 09:55:40 -0300

    adding some elements to storyboard

--- iTale/iTale/ViewController.swift
+++ iTale/iTale/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
 
+    func updateUI() {
+        
+    }
 }
 
