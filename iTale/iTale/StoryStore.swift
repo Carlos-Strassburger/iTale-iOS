8fe7d01f75ba1f73fc972f7b192e47c2411f058c
Carlos Filho <strassburger.filho@gmail.com>
2023-04-05 17:39:41 -0300

    label changes

--- iTale/iTale/StoryStore.swift
+++ iTale/iTale/StoryStore.swift
@@ -12,7 +12,8 @@ struct StoryStore {
     
     let story = [
         Story(storyImage: UIImage(imageLiteralResourceName: "mount-tower"), storyText: "You are a knight on your way to save the Kingdom’s princess from the evil wizard’s tower on top of the mountain. In the foot of the mountain you find yourself in front of a junction, which path do you take?", choice1: "Take the left road", choice2: "Take the right road", choiceDestination1: 2, choiceDestination2: 3),
-        Story(storyImage: UIImage(imageLiteralResourceName: "mountain-road"), storyText: "You take the left road and walk forward, not many minutes later you are ambushed by a burglar, he doesn’t say anything but you can feel he intends to kill you for your equipment, what will you do?", choice1: "Try your luck and attack the assailant", choice2: "Turn around and run away from the bandit", choiceDestination1: 0, choiceDestination2: 0)
+        Story(storyImage: UIImage(imageLiteralResourceName: "mountain-road"), storyText: "You take the left road and walk forward, not many minutes later you are ambushed by a burglar, he doesn’t say anything but you can feel he intends to kill you for your equipment, what will you do?", choice1: "Try your luck and attack the assailant", choice2: "Turn around and run away from the bandit", choiceDestination1: 0, choiceDestination2: 0),
+        Story(storyImage: UIImage(imageLiteralResourceName: "mountain-road"), storyText: "After walking for a long time you are met with yet another junction, this time the left path seems rougher trail and more inclined than the right path, which leads to a softer  climb with a proper road. What road will you take?", choice1: "Try your luck and attack the assailant", choice2: "Turn around and run away from the bandit", choiceDestination1: 0, choiceDestination2: 0)
     ]
     
     var storyNumber = 0
